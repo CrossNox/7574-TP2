8441cc4f7fb6b9c2425e1422865cc9c9afbea9cf
CrossNox <ijmermet@gmail.com>
2022-05-24 00:39:28 -0300

    add filter clis

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [1.3.0] - 2022-05-24
 ### Added
+- Missing CLI interfaces for filters
+
+## [1.3.0] - 2022-05-24
+### Added
 - Join CLI
 
 ## [1.2.0] - 2022-05-23
--- rma/cli/filter.py
+++ rma/cli/filter.py
@@ -3,8 +3,14 @@ from typing import Dict, Union
 import typer
 
 from rma.utils import get_logger
-from rma.tasks.base import VentilatorWorker
-from rma.tasks.filters import FilterEdComment
+from rma.tasks.base import Worker, VentilatorWorker
+from rma.tasks.filters import (
+    FilterNullUrl,
+    FilterEdComment,
+    FilterUniqPosts,
+    FilterNanSentiment,
+    FilterPostsScoreAboveMean,
+)
 
 logger = get_logger(__name__)
 
@@ -14,17 +20,90 @@ state: Dict[str, Union[str, int]] = {}
 
 
 @app.command()
-def ed_comments():
-    worker = VentilatorWorker(**state, executor_cls=FilterEdComment)
+def uniq_posts(
+    subaddr: str,
+    reqaddr: str,
+    pubaddr: str,
+    repaddr: str,
+    nsubs: int = 1,
+):
+    # TODO: this is a limitation design
+    # A ventilator worker should be able to declare dependencies
+    worker = Worker(
+        subaddr=subaddr,
+        reqaddr=reqaddr,
+        pubaddr=pubaddr,
+        subsyncaddr=repaddr,
+        nsubs=nsubs,
+        executor_cls=FilterUniqPosts,
+    )
     worker.run()
 
 
-@app.callback()
-def main(
+@app.command()
+def posts_score_above_mean(
+    subaddr: str,
+    reqaddr: str,
+    pubaddr: str,
+    repaddr: str,
+    meansyncaddr: str,
+    meansubaddr: str,
+    nsubs: int = 1,
+):
+    # TODO: this is a limitation design
+    # A ventilator worker should be able to declare dependencies
+    worker = Worker(
+        subaddr=subaddr,
+        reqaddr=reqaddr,
+        pubaddr=pubaddr,
+        subsyncaddr=repaddr,
+        nsubs=nsubs,
+        executor_cls=FilterPostsScoreAboveMean,
+        deps=[("mean", meansyncaddr, meansubaddr)],
+    )
+    worker.run()
+
+
+@app.command()
+def ed_comments(
     pulladdr: str = typer.Argument(..., help="The address to pull data from"),
     reqaddr: str = typer.Argument(..., help="The address to ack the ventilator"),
     pushaddr: str = typer.Argument(..., help="The address to push data to"),
 ):
-    state["pulladdr"] = pulladdr
-    state["reqaddr"] = reqaddr
-    state["pushaddr"] = pushaddr
+    worker = VentilatorWorker(
+        pulladdr=pulladdr,
+        reqaddr=reqaddr,
+        pushaddr=pushaddr,
+        executor_cls=FilterEdComment,
+    )
+    worker.run()
+
+
+@app.command()
+def nan_sentiment(
+    pulladdr: str = typer.Argument(..., help="The address to pull data from"),
+    reqaddr: str = typer.Argument(..., help="The address to ack the ventilator"),
+    pushaddr: str = typer.Argument(..., help="The address to push data to"),
+):
+    worker = VentilatorWorker(
+        pulladdr=pulladdr,
+        reqaddr=reqaddr,
+        pushaddr=pushaddr,
+        executor_cls=FilterNanSentiment,
+    )
+    worker.run()
+
+
+@app.command()
+def null_url(
+    pulladdr: str = typer.Argument(..., help="The address to pull data from"),
+    reqaddr: str = typer.Argument(..., help="The address to ack the ventilator"),
+    pushaddr: str = typer.Argument(..., help="The address to push data to"),
+):
+    worker = VentilatorWorker(
+        pulladdr=pulladdr,
+        reqaddr=reqaddr,
+        pushaddr=pushaddr,
+        executor_cls=FilterNullUrl,
+    )
+    worker.run()
